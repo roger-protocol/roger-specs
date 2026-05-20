ec456e3d8596b777baf8ffe73d264160cd577d89
OverDsh <overdshdev@gmail.com>
2026-05-20 15:20:49 +0200

    fix: ignore dist and node_modules in eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,9 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default defineConfig(
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
   js.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
@@ -23,8 +26,7 @@ export default defineConfig(
           patterns: [
             {
               group: ["@/features/*"],
-              message:
-                "Core features MUST NOT import from domain specific features",
+              message: "Core features MUST NOT import from domain specific features",
             },
           ],
         },
