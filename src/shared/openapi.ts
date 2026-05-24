21adcf20945e71becffe7618fe1b2b077d854317
OverDsh <overdshdev@gmail.com>
2026-05-24 22:16:18 +0200

    ci(release): create the openapi generator script

---
+++ src/shared/openapi.ts
@@ -0,0 +1,5 @@
+import { extendZodWithOpenApi, OpenAPIRegistry } from "@asteasolutions/zod-to-openapi";
+import z from "zod";
+
+extendZodWithOpenApi(z);
+export const apiRegistry = new OpenAPIRegistry(); // Global OpenAPI registry
