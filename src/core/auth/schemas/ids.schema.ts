fea457d80e44f6caf032b90991941dcd66895a89
OverDsh <overdshdev@gmail.com>
2026-08-25 19:48:50 +0200

    feat: create the UserSession component

--- src/core/auth/schemas/ids.schema.ts
+++ src/core/auth/schemas/ids.schema.ts
@@ -3,22 +3,25 @@ import z from "zod";
 export const UserId = z
   .string()
   .startsWith("rgr_usr_")
-  .openapi({ description: "A unique prefixed user identifier", example: "rgr_usr_9x4hg31..." });
+  .openapi({ description: "A unique prefixed user identifier", example: "rgr_usr_abc123" });
 
 export const JwtId = z
   .string()
   .startsWith("rgr_tok_")
-  .openapi({ description: "A unique prefixed JWT identifier (jti)", example: "rgr_jwt_04gb3w..." });
+  .openapi({ description: "A unique prefixed JWT identifier (jti)", example: "rgr_jwt_abc123" });
 
 export const AuthorizationCode = z.string().startsWith("rgr_ac_").openapi({
   description:
     "A single-use, short-lived authorization code issued by the node after OAuth approval",
-  example: "rgr_ac_8x9f2a11",
+  example: "rgr_ac_abc123",
 });
-export const RefreshToken = z
+
+export const RefreshToken = z.string().startsWith("rgr_rt_").openapi({
+  description: "A long-lived token used to request new short-lived JWTs",
+  example: "rgr_rt_abc123",
+});
+
+export const SessionId = z
   .string()
-  .startsWith("rgr_rt_")
-  .openapi({
-    description: "A long-lived token used to request new short-lived JWTs",
-    example: "rgr_rt_xyz123",
-  });
+  .startsWith("rgr_sess_")
+  .openapi({ description: "A unique session identifier", example: "rgr_sess_abc123" });
---
+++ src/core/auth/schemas/session.schema.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+import { SessionId } from "./ids.schema";
+
+export const UserSession = z
+  .object({
+    id: SessionId,
+    ip_address: z
+      .union([z.ipv4(), z.ipv6()])
+      .openapi({ description: "The last used IP for this session", example: "127.0.0.1" }),
+    is_current: z
+      .boolean()
+      .openapi({ description: "Indicates if this session is the current requesting session" }),
+    country: z
+      .string()
+      .optional()
+      .openapi({ description: "The country where the session was last used", example: "France" }),
+    client_name: z.string().openapi({
+      description:
+        "The user-friendly name of the client who created the session (can be spoofable)",
+      example: "Roger CLI (MacOS)",
+    }),
+    created_at: z.iso.datetime().openapi({
+      description: "The ISO timestamp at which the session was created",
+      example: "2026-08-22T12:14:03.534Z",
+    }),
+    last_used_at: z.iso.datetime().openapi({
+      description: "The ISO timestamp at which the session was last used",
+      example: "2026-08-25T16:50:33.444Z",
+    }),
+  })
+  .openapi("UserSession");
