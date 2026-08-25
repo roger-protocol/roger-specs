f7d659ebf0a9fa6db7413e3b75223b2d96758aa4
OverDsh <overdshdev@gmail.com>
2026-08-25 11:12:52 +0200

    build: include schemas in the npm package

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -1,3 +1,9 @@
+// Routes
 import "./routes/oauth.route";
 import "./routes/token.route";
 import "./routes/revoke.route";
+
+// Schemas
+import "./schemas/callback.schema";
+import "./schemas/ids.schema";
+import "./schemas/jwt.schema";
